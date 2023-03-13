fd2f5da6d5f46d063a502a8ef7261132e6f0f125
zonitidus <zonitidus@gmail.com>
2023-03-13 10:08:41 -0500

    feat: add initial python backend to upload both index and nodes config files.

---
+++ 2023-1/webapp/webapp.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import os
+
+app = Flask(__name__, template_folder='template')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+
+    username = request.form['username']
+
+    file1 = request.files['file1']
+    file2 = request.files['file2']
+
+    os.mkdir(username)
+
+    file1.save(f'{username}/{file1.filename}')
+    file2.save(f'{username}/{file2.filename}')
+    return "Files uploaded successfully"
+
+if __name__ == '__main__':
+    app.run(debug=True)
